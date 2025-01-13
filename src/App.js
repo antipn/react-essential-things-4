930cc0e42ad325bac9d6398ce9c4fd533081237a
Nikolay <palmer@bk.ru>
2025-01-13 15:06:06 +0300

    BASIC FORM PROVIDED

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import InputForm from "./components/InputForm";
+import BasicForm from "./components/BasicForm";
+
+function App() {
+    return (
+        <div className="app">
+            {/*<InputForm/>*/}
+            <BasicForm/>
+        </div>
+    );
+}
+
+export default App;
