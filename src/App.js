96aaf041a9d890b913011f6a6872b1b0e4b62744
Nikolay <palmer@bk.ru>
2025-01-13 15:09:15 +0300

    BASIC FORM PROVIDED

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
     return (
         <div className="app">
             {/*<InputForm/>*/}
+            <div className='form-title'>New User Input Form</div>
             <BasicForm/>
         </div>
     );
