5079556fbaa8a9ff5dd3819c747473dcc638c428
Nikolay <palmer@bk.ru>
2025-01-13 15:26:25 +0300

    BASIC FORM PROVIDED + useReducer

--- src/hooks/use-input.js
+++ src/hooks/use-input.js
@@ -1,30 +1,56 @@
-import {useState} from "react";
+import {useReducer, useState} from "react";
+
+const initialInputState = {
+    value: '',
+    isTouched: false
+}
+
+const inputStateReducer = (state, action) => {
+    if (action.type === 'INPUT') {
+        return {value: action.value, isTouched: state.isTouched};
+    }
+    if (action.type === 'BLUR') {
+        return {isTouched: true, value: state.value};
+    }
+    if (action.type === 'RESET') {
+        return {value: '', isTouched: false};
+    }
+
+
+    return inputStateReducer;
+}
 
 const useInput = (validateValue) => {
 
-    const [enteredValue, setEnteredValue] = useState('');
-    const [isTouched, setIsTouched] = useState(false);
+    const [inputState, dispatch] = useReducer(inputStateReducer, initialInputState);
+
+    // const [enteredValue, setEnteredValue] = useState('');
+    // const [isTouched, setIsTouched] = useState(false);
 
     //валидация через фунцию что сюда придет!
-    const valueIsValid = validateValue(enteredValue);
-    const hasError = !valueIsValid && isTouched;
+    const valueIsValid = validateValue(inputState.value);
+    const hasError = !valueIsValid && inputState.isTouched;
 
     const valueChangeHandler = (event) => {
-        setEnteredValue(event.target.value);
+        dispatch({type: 'INPUT', value: event.target.value});
+        //setEnteredValue(event.target.value);
     }
 
     const inputBlurHandler = (event) => {
-        setIsTouched(true);
+
+        dispatch({type: 'BLUR'});
+        //setIsTouched(true);
     }
 
     const reset = () => {
-        setEnteredValue('');
-        setIsTouched(false);
+        dispatch({type: 'RESET'});
+        //setEnteredValue('');
+        //setIsTouched(false);
     }
 
     //возвращаем объект
     return {
-        value: enteredValue,
+        value: inputState.value,
         isValid: valueIsValid,
         hasError,
         valueChangeHandler,
